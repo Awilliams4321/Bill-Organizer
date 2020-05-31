699964d604c57fc914bb1f0232cd9b18f53bfeb7
Awilliams4321 <awilliams1234@live.com>
2020-05-31 00:56:16 -0600

    Redirecting new users to the 'users/show' page.

--- bill-organizer/app/controllers/users_controller.rb
+++ bill-organizer/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         new_user = User.new(username: params[:username], email: params[:email], password: params[:password])
         if new_user.save
             session[:user_id] = new_user.id
-            redirect to 'users/#{new_user.id}'
+            redirect to 'users/show'
         else
             redirect to 'users/signup'
         end
