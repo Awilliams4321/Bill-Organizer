8257e7813aed0cf4bf85b4dabcc3f420b5ea71a5
Awilliams4321 <awilliams1234@live.com>
2020-05-31 02:39:09 -0600

    Creating a POST request that creates a new bill.

--- bill-organizer/app/controllers/bills_controller.rb
+++ bill-organizer/app/controllers/bills_controller.rb
@@ -4,5 +4,15 @@ class BillsController < ApplicationController
         erb :'bills/new'
     end
 
+    post '/bills' do
+        @bill = Bill.create(
+            name: params[:name],
+            creditor: params[:creditor],
+            balance_owed: params[:balance_owed],
+            monthly_payment: params[:monthly_payment],
+            due_date: params[:due_date]
+        )
+    end 
+
 
 end
