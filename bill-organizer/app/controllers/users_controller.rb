c43905ff7a6fb828bea5bbd94ffb3a9d18e9f9ac
Awilliams4321 <awilliams1234@live.com>
2020-05-31 00:45:22 -0600

    Creating a POST route that will create and save a new user if the params are filled outand then redirect to the users sign in page. If a user has not signed in properly, they will be redirected to the sign up page.

--- bill-organizer/app/controllers/users_controller.rb
+++ bill-organizer/app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
         erb :'users/new' 
     end
 
+    post '/users' do 
+        new_user = User.new(email: params[:email], password: params[:password])
+        if new_user.save
+            session[:user_id] = new_user.id
+            redirect to 'users/#{new_user.id}'
+        else
+            redirect to 'users/signup'
+        end
+    end
+
 end
