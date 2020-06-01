602f5138a5f1819da7099b06cf23b3b190f16e20
Awilliams4321 <awilliams1234@live.com>
2020-05-31 19:39:17 -0600

    Creating a new sessions/show route that will redirect my return users to their page.

--- bill-organizer/app/controllers/sessions_controller.rb
+++ bill-organizer/app/controllers/sessions_controller.rb
@@ -4,6 +4,22 @@ class SessionsController < ApplicationController
          erb :'sessions/new'
     end
 
+    get "/sessions/show" do
+        erb :'/sessions/show'
+    end
+
+
+    post '/login' do 
+        @new_user = User.find_by(email: params[:email])
+
+        if @new_user && @new_user.authenticate(params[:password])
+            session[:user_id] = @new_user.id
+            redirect "/sessions/show"
+        else
+            erb :'/sessions/new'
+        end
+    end 
+
     get '/logout' do
         session.clear
     end
