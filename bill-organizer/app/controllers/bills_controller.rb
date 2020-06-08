1df84ad65d2093ed61c0ce83dc892fc6d682bcda
Awilliams4321 <awilliams1234@live.com>
2020-06-08 09:57:39 -0600

    Adding validation lines to all methods that require user interaction.

--- bill-organizer/app/controllers/bills_controller.rb
+++ bill-organizer/app/controllers/bills_controller.rb
@@ -1,53 +1,62 @@
 class BillsController < ApplicationController
 
     get '/bills/new' do 
+        redirect to "/" if !is_logged_in?
+
         erb :'bills/new'
     end
 
     post '/bills' do #CREATE
-        @bill = Bill.create(
+        @bill = Bill.new(
             name: params[:name],
             creditor: params[:creditor],
             balance_owed: params[:balance_owed],
             monthly_payment: params[:monthly_payment],
-            due_date: params[:due_date]
+            due_date: params[:due_date],
+            user_id: current_user.id
         )
-        redirect "/bills/#{@bill.id}"
+        if @bill.save
+            redirect "/bills/#{@bill.id}"
+        else
+             erb :'/bills/new'
+        end
     end 
 
     get '/bills/:id' do #READ
+        redirect to "/" if !is_logged_in?
         @bill = Bill.find(params[:id])
         erb :'/bills/show'
     end
 
     get '/bills' do 
+        redirect to "/" if !is_logged_in?
         @bills = Bill.all
         erb :'bills/index'
     end
 
     get '/bills/:id/edit' do #UPDATE
+        redirect to "/" if !is_logged_in?
         @bill = Bill.find(params[:id])
         erb :'bills/edit'
     end 
 
-    patch '/bills/:id' do
+    patch '/bills/:id' do #Add 
+        redirect to "/" if !is_logged_in?
         @bill = Bill.find(params[:id])
         @bill.update(
             name: params[:name],
             creditor: params[:creditor],
-            balance_owed: params[:balance_owed],
+            balance_owed: params[:balance_owed], #if block 0 if nil or empty
             monthly_payment: params[:monthly_payment],
             due_date: params[:due_date]
         )
         redirect "/bills/#{@bill.id}"
-
     end 
 
     delete '/bills/:id' do #DELETE
-        @bill = Bill.find(params[:id])
-        @bill.destroy
+        bill = Bill.find(params[:id])
+        bill.destroy if bill && bill.user_id == current_user.id
         redirect "/bills"
     end
-
 
 end
