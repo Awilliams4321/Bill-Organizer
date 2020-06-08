ad153d1829f404e2b67e287d7a1827610867c982
Awilliams4321 <awilliams1234@live.com>
2020-06-08 10:01:04 -0600

    Adding a user_id column to my bills table.

--- bill-organizer/db/schema.rb
+++ bill-organizer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200531024610) do
+ActiveRecord::Schema.define(version: 20200608145030) do
 
   create_table "bills", force: :cascade do |t|
     t.string  "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20200531024610) do
     t.integer "balance_owed"
     t.integer "monthly_payment"
     t.integer "due_date"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
