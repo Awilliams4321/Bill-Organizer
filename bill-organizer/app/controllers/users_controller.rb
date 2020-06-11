67bd269ae8de842d5e540085cf6ce900b2c67f1a
Awilliams4321 <awilliams1234@live.com>
2020-06-10 18:42:44 -0600

    Making my if statement in my dynamic route a one liner.

--- bill-organizer/app/controllers/users_controller.rb
+++ bill-organizer/app/controllers/users_controller.rb
@@ -5,17 +5,18 @@ class UsersController < ApplicationController
     end
 
     post '/users' do 
-        if params[:email] == "" && params[:password] == ""
-            #flash[:error] = "Invalid Email or Password. Please Try Again."
-            redirect "/users/signup"
-        else
-            @new_user = User.create(email: params[:email], password: params[:password])
+        @new_user = User.new(email: params[:email], password: params[:password])
+        if @new_user.save
             session[:user_id] = @new_user.id
             redirect "/users/#{@new_user.id}" 
+        else
+            #flash[:error] = "Invalid Email or Password. Please Try Again."
+            redirect "/users/signup"
         end
     end
 
     get '/users/:id' do
+        redirect to "/" if !is_logged_in?
         @new_user = User.find(params[:id])
         erb :'users/show'
     end 
